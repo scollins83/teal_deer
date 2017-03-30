8855a85040d1bf4dc0c451db43b7746dc29f68a7
Sara Collins <datadivallc@gmail.com>
2017-03-30 17:09:07 -0500

    Working code and test for changing file types in output file name.

--- teal_deer.py
+++ teal_deer.py
@@ -1,10 +1,43 @@
 import glob
+import argparse
+import json
 
-def get_file_list(path, type='pdf'):
+
+def get_file_list(path, file_type='pdf'):
     """
     Gets a list of files of the directory of pdfs.
     :param path: Directory path
+    :param file_type: Type of file to be imported.
     :return: List of files.
     """
-    path += ('*.' + type)
+    path += ('*.' + file_type)
     return glob.glob(path)
+
+
+def parse_args():
+    """
+    Returns arguments passed at the command line as a dict
+    """
+    parser = argparse.ArgumentParser(description='Generates a machine Learning Dataset.')
+    parser.add_argument('-c', help="Config File Location", required=True,
+                        dest='config')
+    args = vars(parser.parse_args())
+    return args
+
+
+def load_config(config_name):
+    """
+    loads a json config file and returns a config dictionary
+    """
+    with open(config_name) as config_file:
+        config = json.load(config_file)
+        return config
+
+
+def replace_file_type_in_file_name(file_path, input_type='pdf', output_type='txt'):
+    """
+    Replace one file type with another in the specified file name.
+    :param file_path: Starting file path.
+    :return: File name with replacement type.
+    """
+    return file_path.replace('.' + input_type, '.' + output_type)
--- test_tldr.py
+++ test_tldr.py
@@ -1,5 +1,20 @@
 # Nose tests for python.
 import teal_deer as tldr
 
+
+def test_get_configuration():
+    assert len(tldr.load_config('config.json')) == 3, "Configuration not loaded correctly."
+
+
 def test_get_file_list():
-    assert len(tldr.get_file_list(TD_PDF_FILEPATH)) == 106, "Getting incorrect number of files."
+    configuration = tldr.load_config('config.json')
+    assert len(tldr.get_file_list(configuration['files_path'])) == 106, "Getting incorrect number of files."
+
+
+def test_replace_file_type_in_file_name():
+    configuration = tldr.load_config('config.json')
+    file_list = tldr.get_file_list(configuration['files_path'])
+    print(file_list[0])
+    assert tldr.replace_file_type_in_file_name(file_list[0]) == \
+           '/Users/saracollins/Documents/LiteratureReviews/chatbots/0310018.txt', \
+        "File type replacement not happening correctly."
